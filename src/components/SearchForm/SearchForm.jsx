ffe0f91402ffbcebdc6edc4aa5e70a8ad238d860
Utilizator <Stanislavbox24@gmail.com>
2023-06-08 20:18:36 +0300

    add serchGallery

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,30 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    value: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({value: e.target.value})
+  }
+
   render() {
-    return <h2>SearchForm</h2>;
+    const {value} = this.state
+    return (
+      <SearchFormStyled>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          onChange={this.handleChange}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          value={value}
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,13 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  componentDidMount(){
+    ImageService.getImages('car', 1)
+  }
   render() {
     return (
       <>
+      <SearchForm />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
